c30c508b24b9da3bea9a5009e01dfff742a6b9c0
Schmaiki <maikpeters1511@gmail.com>
2022-01-13 13:45:17 +0100

    Projekt

---
+++ LCD1602/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Threading;
+using Iot.Device.CharacterLcd;
+using Iot.Device.Pcx857x;
+
+Console.WriteLine("Displaying current time. Press Ctrl+C to end.");
+
+using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
+using var driver = new Pcf8574(i2c);
+using var lcd = new Lcd1602(registerSelectPin: 0, 
+                        enablePin: 2, 
+                        dataPins: new int[] { 4, 5, 6, 7 }, 
+                        backlightPin: 3, 
+                        backlightBrightness: 0.1f, 
+                        readWritePin: 1, 
+                        controller: new GpioController(PinNumberingScheme.Logical, driver));
+int currentLine = 0;
+
+    lcd.Clear();
+    lcd.SetCursorPosition(0,currentLine);
+    lcd.Write("Uhrzeit:");
+
+while (true)
+{
+    lcd.SetCursorPosition(0,1);
+    lcd.Write(DateTime.Now.ToShortTimeString());
+}
+
