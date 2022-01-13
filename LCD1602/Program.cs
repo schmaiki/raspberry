49ed3cb1ae28e1ad7e274dff6765f9c0cf2464c7
Schmaiki <maikpeters1511@gmail.com>
2022-01-13 14:27:45 +0100

    Init Sensor und Funktionen eingefügt Ausgabe aufs LCD

--- LCD1602/Program.cs
+++ LCD1602/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.WriteLine("Displaying current time. Press Ctrl+C to end.");
+﻿Console.WriteLine("Programm Startet!!!. Press Ctrl+C to end.");
 
+//Init Display
 using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x27));
 using var driver = new Pcf8574(i2c);
 using var lcd = new Lcd1602(registerSelectPin: 0,
@@ -9,14 +10,31 @@ using var lcd = new Lcd1602(registerSelectPin: 0,
     backlightBrightness: 0.1f,
     readWritePin: 1,
     controller: new GpioController(PinNumberingScheme.Logical, driver));
-int currentLine = 0;
 
-lcd.Clear();
-lcd.SetCursorPosition(0, currentLine);
-lcd.Write("Uhrzeit:");
+//Init BME280 Senor
+var i2cSettings = new I2cConnectionSettings(1, Bme280.DefaultI2cAddress);
+using I2cDevice i2cDevice = I2cDevice.Create(i2cSettings);
+using var bme280 = new Bme280(i2cDevice);
+
+int measurementTime = bme280.GetMeasurementDuration();
+
+int currentLine = 0;
+int secondLine = 1;
 
 while (true)
 {
-    lcd.SetCursorPosition(0, 1);
-    lcd.Write(DateTime.Now.ToShortTimeString());
+    Console.Clear();
+    lcd.Clear();
+    bme280.SetPowerMode(Bmx280PowerMode.Forced);
+    Thread.Sleep(measurementTime);
+
+    bme280.TryReadTemperature(out var tempValue);
+    bme280.TryReadHumidity(out var humValue);
+
+    lcd.SetCursorPosition(0, currentLine);
+    lcd.Write($"Temp: {tempValue.DegreesCelsius:0.#} C");
+    lcd.SetCursorPosition(0, secondLine);
+    lcd.Write($"Luft: {humValue.Percent:#.##}%");
+
+    Thread.Sleep(5000);
 }
