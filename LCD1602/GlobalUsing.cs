1e6052f9999fc18bfb9cec08bed326bf8161f106
Schmaiki <maikpeters1511@gmail.com>
2022-01-13 13:50:34 +0100

    GlobalUsing Klasse angelegt

---
+++ LCD1602/GlobalUsing.cs
@@ -0,0 +1,11 @@
+global using System;
+global using System.Device.Gpio;
+global using System.Device.I2c;
+global using System.Threading;
+global using Iot.Device.CharacterLcd;
+global using Iot.Device.Pcx857x;
+global using Iot.Device.Bmxx80;
+global using Iot.Device.Bmxx80.PowerMode;
+global using Iot.Device.Bmxx80.PowerMode;
+
+namespace LCD1602;
